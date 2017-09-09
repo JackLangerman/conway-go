72628e27f3146e844945c6f4ee51ceb0d09a4b44
JackLangerman <JackMLangerman@gmail.com>
2017-09-09 11:31:30 -0400

    clean and comment

--- conway.go
+++ conway.go
@@ -5,119 +5,88 @@ import (
     "fmt"
     "log"
     "os"
-    //"io"
 )
 
+// struct to hold the current gamestate
 type Board struct {
 	board [][]bool
 	width, height int
 }
 
-func (board *Board) importStartStateScanner() {
+// constructor for the board struct
+func createBoard(width, height int) *Board {
+	board := make([][]bool, height+2)				// add extra row above and below board to avoid checking boundries
+	for row := range board {
+		board[row] = make([]bool, width+2) 			// add extra col left and right of board to avoid checking boundries
+	}
+	return &Board{board: board, width: width, height: height}
+}
 
+//function to read the startstate of the game from a file to the Board struct
+func (board *Board) importStartStateScanner(filename string) {
 
-	file, err := os.Open("life.txt")
-    if err != nil {
+	
+	file, err := os.Open(filename) 		// open file for reading
+    if err != nil { 			   		// if the file does not exist log and exit
         log.Fatal(err)
     }
-    defer file.Close()
+    defer file.Close()			   		// close this file when the function returns
 
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanRunes)
+    scanner := bufio.NewScanner(file)	// create a new scanner to read the file
+    scanner.Split(bufio.ScanRunes)    	// tell the scanner to split on characters
 
-    row := 0
+    row := 0							// make some variable for looping through rows/cols
     col := -1
-    for scanner.Scan() {
-        output_char := scanner.Text()
+    for scanner.Scan() {				// while there are more characters in the file 
+        output_char := scanner.Text()	// get the next character
         
-        col++
+        col++							// increment the col variable
 
-        if output_char == "*" {
+        if output_char == "*" {			// if the curent char is an astrix mark the cell as alive
         	board.setPosition(row, col, true)
-        } else if output_char == "\n"{
+        } else if output_char == "\n" {	// if the cur char is a newline increment the row and reset the col
         	row++
         	col = -1
         }
-
-        // fmt.Print(output_char)
     }
 
-    if err := scanner.Err(); err != nil {
+    if err := scanner.Err(); err != nil { // check for errors in the scanner
         log.Fatal(err)
     }
-    // fmt.Println("\n\n")
-    board.print()
-    // return nil
+
 }
 
+// function to print the current gamestate to standard output 
 func (board *Board) print() {
-	for i := 0 ; i < board.height ; i++ {
-		for j := 0 ; j < board.width ; j++ {
-			if board.isAlive(i, j) {
-				fmt.Print("*")
+	for i := 0 ; i < board.height ; i++ {		// for each row
+		for j := 0 ; j < board.width ; j++ {	// for each col
+			if board.isAlive(i, j) {			// check if the curent cell is alive
+				fmt.Print("*")					// if so print "*"
 			} else {
-				fmt.Print("-")
+				fmt.Print(" ")					// else print " "
 			}
 		}
 		fmt.Print("\n")
 	}
 }
 
+//function to check if a particular cell is alive
 func (board *Board) isAlive(row, col int) bool {
 	return board.board[row+1][col+1]
 }
 
+//function to set the state of a particular cell (alive / dead)
 func (board *Board) setPosition(row int, col int, alive bool) *Board {
 	board.board[row+1][col+1] = alive
 	return board
 }
 
 
-func createBoard2020() *Board {
-	return createBoard(20, 20)
-}
-
-func createBoard(width, height int) *Board {
-	board := make([][]bool, height+2)
-	for row := range board {
-		board[row] = make([]bool, width+2)
-	}
-	return &Board{board: board, width: width, height: height}
-}
-
-func addLine(board *Board, line *[]bool, lineNumber int) *Board {
-	board.board[lineNumber+1] = *line
-	return board
-}
-
-
-// func importStartStateReader() *Board {
-// 	board := createBoard2020()
-
-// 	f, err := os.Open("life.txt")
-//     if err != nil {
-//             log.Fatal(err)
-//     }
-
-//     lineReader := bufio.NewReader(f)
-//     for i:=0 ; ; i++ {
-//         if sliceLine, err := lineReader.ReadSlice('\n'); err == nil || err == io.EOF {
-//                 //fmt.Printf("%s", sliceLine)
-//         		addLine(board, &sliceLine, i)
-//                 if err == io.EOF {
-//                         break
-//                 }
-//         } else {
-//                 log.Fatal(err)
-//         }
-
-//     }
-//     return board
-// }
 
 func main() {
 	board := createBoard(20, 8)
-    board.importStartStateScanner()
+    board.importStartStateScanner("life.txt")
+    board.print()
 }
 
 
