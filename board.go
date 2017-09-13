6ba69a69fc726031dff0992e7fa5fbf50443037a
JackLangerman <JackMLangerman@gmail.com>
2017-09-13 02:08:58 -0400

    generalize import

--- board.go
+++ board.go
@@ -7,13 +7,13 @@ import (
     "os"
 )
 
-// struct to hold the current gamestate
+// Board is a struct to hold the current gamestate
 type Board struct {
 	board [][]bool
 	width, height int
 }
 
-// constructor for the board struct
+// createBoard is the constructor for the board struct.
 func createBoard(width, height int) Board {
 	board := make([][]bool, height+2)				// add extra row above and below board to avoid checking boundries
 	for row := range board {
@@ -22,7 +22,7 @@ func createBoard(width, height int) Board {
 	return Board{board: board, width: width, height: height}
 }
 
-
+// importBoard imports the seed state for the board from the file named "filename".
 func importBoard(filename string) Board {
 	file, err := os.Open(filename) 		// open file for reading
     if err != nil { 			   		// if the file does not exist log and exit
@@ -30,170 +30,65 @@ func importBoard(filename string) Board {
     }
     defer file.Close()			   		// close this file when the function returns
 
-    var width, height int
-    var board [][]bool
-
+    var width, height int 				// make some vars for board width and height
+    var board [][]bool					// make an array to hold the board
 	
 
     scanner := bufio.NewScanner(file)	// create a new scanner to read the file
-    for scanner.Scan() {	
-    	line := scanner.Text()
-    	// fmt.Println(len(line))
-    	// fmt.Println(line)
-    	// fmt.Println("|||||||||||")
-    	width = 0
+    for scanner.Scan() {				// make a line scanner to read the file a line at a time
+    	line := scanner.Text()			// read a line of the file
 
-   		var row []bool
-   		row = append(row, false)
+    	width = 0						// set the line width to 0
 
-    	for _, c := range line {
-    		// fmt.Printf("%c", c)
-    		b := c=='*'
-    		row = append(row, b)
-    		width++
-    		// board = append(board, row)
-    		// board[height] = append(board[height], b)
+   		var row []bool					// make a slice for the current row
+   		row = append(row, false)		// add a dead cell at the beggining of the row
+
+    	for _, c := range line {		// loop through each char of the current line in the file
+    		b := c=='*'					// check if there is a *(to indicate life) in the cur cell
+    		row = append(row, b)		// add the cell state to the end of the row
+    		width++						// increment the width
     	}
-    	row = append(row, false)
+    	row = append(row, false)		// add a dead cell at the end of the row
 
-    	board = append(board, row)
-    	height++
-    	// fmt.Println(width, height)
-    	// fmt.Println(board)
-    	// fmt.Println()
+    	board = append(board, row)		// add the current row to the end of the board
+    	height++						// increment the height
     }
-    // row := make([]bool, width)
-    // rrow := make([][]bool, 1)
-    // rrow[0] = row
-    // fmt.Println(reflect.TypeOf(rrow))
-    // fmt.Println(reflect.TypeOf(board))
+
     board = append(make([][]bool, 1), board...)
-    board[0] = append(make([]bool, width+2))
+    board[0] = append(make([]bool, width+2))		// prepend a dead row the the board
+
     board = append(board, make([][]bool, 1)...)
-    board[height+1] = append(make([]bool, width+2))
-    // fmt.Println("|||||||||||||||\n", len(board), "\n|||||||||||||||")
-    // fmt.Println("|||||||||||||||\n", len(board[0]), "\n|||||||||||||||")
-    // fmt.Println("|||||||||||||||\n", len(board[len(board)]), "\n|||||||||||||||")
-    for _, row := range board {
-    	fmt.Println(row)
-    }
+    board[height+1] = append(make([]bool, width+2)) // append a dead row to the board
+
     return Board{board: board, width: width, height: height}
 }
 
-
-
-
-// read the startstate of the game from a file to the Board struct
-func importBoardd(filename string) Board {
-	var board [][]bool
-	width := 0
-	height := 0
-	// board = make([][]bool, height)
-	// board = append(board, )
-	// board.append(make([]bool, 1))
-
-	file, err := os.Open(filename) 		// open file for reading
-    if err != nil { 			   		// if the file does not exist log and exit
-        log.Fatal(err)
-    }
-    defer file.Close()			   		// close this file when the function returns
-
-    scanner := bufio.NewScanner(file)	// create a new scanner to read the file
-    scanner.Split(bufio.ScanRunes)    	// tell the scanner to split on characters
-
-    row := 0							// make some variable for looping through rows/cols
-    col := -1
-
-    board = append(board, make([]bool, width))
-
-    for scanner.Scan() {				// while there are more characters in the file 
-    	board = append(board, make([]bool, width))
-        output_char := scanner.Text()	// get the next character
-        
-        col++							// increment the col variable
-
-        board[row] = append(board[row], true)
-
-        if output_char == "*" {			// if the curent char is an astrix mark the cell as alive
-        	// board.setPosition(row, col, true)
-        	board[row] = append(board[row], true)
-        	fmt.Print(board[row][col], " ")
-        } else if output_char == "\n" {	// if the cur char is a newline increment the row and reset the col
-        	fmt.Println(board[row][col], " ")
-        	width = col
-        	row++
-        	col = -1
-        } else {
-        	board[row] = append(board[row], false)
-        	fmt.Print(board[row][col], " ")
-        }
-        height = row
-    }
-
-    if err := scanner.Err(); err != nil { // check for errors in the scanner
-        log.Fatal(err)
-    }
-    return Board{board: board, width: width, height: height}
-}
-
-// read the startstate of the game from a file to the Board struct
-func (board *Board) importStartStateScanner(filename string) {
-	file, err := os.Open(filename) 		// open file for reading
-    if err != nil { 			   		// if the file does not exist log and exit
-        log.Fatal(err)
-    }
-    defer file.Close()			   		// close this file when the function returns
-
-    scanner := bufio.NewScanner(file)	// create a new scanner to read the file
-    scanner.Split(bufio.ScanRunes)    	// tell the scanner to split on characters
-
-    row := 0							// make some variable for looping through rows/cols
-    col := -1
-    for scanner.Scan() {				// while there are more characters in the file 
-        output_char := scanner.Text()	// get the next character
-        
-        col++							// increment the col variable
-
-        if output_char == "*" {			// if the curent char is an astrix mark the cell as alive
-        	board.setPosition(row, col, true)
-        } else if output_char == "\n" {	// if the cur char is a newline increment the row and reset the col
-        	row++
-        	col = -1
-        }
-    }
-
-    if err := scanner.Err(); err != nil { // check for errors in the scanner
-        log.Fatal(err)
-    }
-
-}
-
-//  print the current gamestate to standard output 
+//  print prints the current gamestate to standard output.
 func (board *Board) print() {
 	for i := 0 ; i < board.height ; i++ {		// for each row
 		for j := 0 ; j < board.width ; j++ {	// for each col
 			if board.isAlive(i, j) {			// check if the curent cell is alive
 				fmt.Print("*")					// if so print "*"
 			} else {
-				fmt.Print("-")					// else print " "
+				fmt.Print(" ")					// else print " "
 			}
 		}
 		fmt.Print("\n")
 	}
 }
 
-// check if a particular cell is alive
+// isAlive checks if the cell at (row, col) is alive.
 func (board *Board) isAlive(row, col int) bool {
 	return board.board[row+1][col+1]
 }
 
-// set the state of a particular cell (alive / dead)
+// setPosition set the state of the cell at (row, col) to the bool "alive".
 func (board *Board) setPosition(row int, col int, alive bool) *Board {
 	board.board[row+1][col+1] = alive
 	return board
 }
 
-// count how many of cell at (row, col)'s eight neighbors are alive
+// countNeighbors counts how many of cell at (row, col)'s eight neighbors are alive.
 func (board *Board) countNeighbors(row, col int) int {
 	count := 0						// innitalize a var to count living  neighbors
 	for i := -1; i <= 1; i++ {		// check above and below
@@ -207,8 +102,7 @@ func (board *Board) countNeighbors(row, col int) int {
 	return count
 }
 
-
-// advance the board to the next state
+// nextState advances the board to the next state.
 func (board *Board) nextState() {
 	nextboard := createBoard(board.width, board.height)		// create a board to store the next state
 
@@ -226,6 +120,5 @@ func (board *Board) nextState() {
 		}
 	}
 
-	// board.update(nextboard)										 // update the board with the new state
 	*board = nextboard
 }
--- conway.go
+++ conway.go
@@ -4,7 +4,7 @@ import (
     "fmt"
 )
 
-const numGenerations = 10
+const numGenerations = 1000
 
 func main() {
 	// define the board size and number generations to simulate
@@ -14,7 +14,7 @@ func main() {
 
 	// board := createBoard(boardWidth, boardHeight)		// create the board
     // board.importStartStateScanner("life.txt")		    // read the seed state from life.txt
-    board := importBoard("life.txt")
+    board := importBoard("life3.txt")
 
     for i := 0 ; i <= numGenerations ; i++ {			// advance numGenerations
     	fmt.Printf("\nState #%d\n", i)					// print state #
