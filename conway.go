eb5fb32a360b3d2a86ef92d3ae680a13107d05e7
JackLangerman <JackMLangerman@gmail.com>
2017-09-13 02:13:24 -0400

    generalize import

--- conway.go
+++ conway.go
@@ -4,16 +4,9 @@ import (
     "fmt"
 )
 
-const numGenerations = 1000
+const numGenerations = 1005
 
 func main() {
-	// define the board size and number generations to simulate
-	// boardWidth := 20
-	// boardHeight := 8
-	// numGenerations := 10
-
-	// board := createBoard(boardWidth, boardHeight)		// create the board
-    // board.importStartStateScanner("life.txt")		    // read the seed state from life.txt
     board := importBoard("life3.txt")
 
     for i := 0 ; i <= numGenerations ; i++ {			// advance numGenerations
